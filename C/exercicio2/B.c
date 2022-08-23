c46ee838be8d8573305e94a219cde2075daa44fb
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-08-23 13:36:17 +0000

    Tue, Aug 23, 2022, 1:36 PM +00:00

--- C/exercicio2/B.c
+++ C/exercicio2/B.c
@@ -2,10 +2,22 @@
 
 int main(void)
 {
-	int tipo_tinta = 0;
+	int tipo_tinta = 0, mtros_quad = 0;
 
 	printf("Qual o tipo de tinta a ser usada?(\"0\" para acrílica e \"1\" para lavável):");
 	scanf("%d", &tipo_tinta);
-	
+
+	printf("Quantos metros quadrados serão pintados?:");
+	scanf("%d", &mtros_quad);
+
+	//Acrílica
+	if(tipo_tinta == 0)
+	{
+		printf("Custo: R$%.2f\n", 10.0*mtros_quad);
+	}
+	else
+	{
+		printf("Custo: R$%.2f\n", 15.0*mtros_quad);
+	}
 	return 0;
 }
