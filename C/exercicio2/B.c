0fb94e2295336a927eaf82da66ff380939df3d6a
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-08-23 13:27:15 +0000

    Tue, Aug 23, 2022, 1:27 PM +00:00

--- C/exercicio2/B.c
+++ C/exercicio2/B.c
@@ -4,6 +4,8 @@ int main(void)
 {
 	int tipo_tinta = 0;
 
-	printf("Qual o tipo de tints a ser usada? 0 para")
+	printf("Qual o tipo de tinta a ser usada?(\"0\" para acrílica e \"1\" para lavável):");
+	scanf("%d", &tipo_tinta);
+	
 	return 0;
 }
