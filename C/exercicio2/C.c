772707bb397fbdb1e922afe531e680ed30694386
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-08-23 13:47:06 +0000

    Tue, Aug 23, 2022, 1:47 PM +00:00

--- C/exercicio2/C.c
+++ C/exercicio2/C.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
+int ler_int(char*);
+
 int main(void)
 {
-	int rqr_mtral = 0, n_irms = 0;
+	int rqr_mtral =ler_int("");
+	int n_irms = 0;
+	int venceu = ler_int("O boleto foi pago antes do vencimento? (\"0\" para não e \"1\" para sim)");
 	return 0;
+}
+
+int ler_int(char *txto)
+{
+	int n = 0;
+	printf(txto);
+	scanf("%d" &n);
+
+	return n;
 }
