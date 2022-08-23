bbcbd27859a36d1846c7907764291e4c5a87fba6
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-08-23 13:13:20 +0000

    Tue, Aug 23, 2022, 1:13 PM +00:00

--- C/exercicio2/A.c
+++ C/exercicio2/A.c
@@ -3,7 +3,15 @@
 int main(void)
 {
 	float vlor = 0;
+	//Preço da compra
+	printf("Preço:");
+	scanf(%f, vlor);
+	printf("\n");
+	if(vlor > 100)
+	{
+		vlor = vlor*0.9;
+	}
 
-	printf("Valor a ser pago: R$%2f\n");
+	printf("Valor a ser pago: R$%2f\n", vlor);
 	return 0;
 }
