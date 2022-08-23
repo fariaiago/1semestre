19cf0d1ed16a69c9b60a0ac7218c5d0bc6089700
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-08-23 13:56:42 +0000

    Tue, Aug 23, 2022, 1:56 PM +00:00

--- C/exercicio2/B.c
+++ C/exercicio2/B.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 
+int ler_int(char*);
+
 int main(void)
 {
-	int tipo_tinta = 0, mtros_quad = 0;
-
-	printf("Qual o tipo de tinta a ser usada?(\"0\" para lavável e \"1\" para acrílica):");
-	scanf("%d", &tipo_tinta);
-
-	printf("Quantos metros quadrados serão pintados?:");
-	scanf("%d", &mtros_quad);
+	int tipo_tinta = ler_int("Qual o tipo de tinta a ser usada?(\"0\" para lavável e \"1\" para acrílica): ");
+	int area = ler_int("Quantos metros quadrados serão pintados?: ");
 
 	//Acrílica
 	if(tipo_tinta)
 	{
-		printf("Custo: R$%.2f\n", 10.0*mtros_quad);
+		printf("Custo: R$%.2f\n", 10.0*area);
 	}
 	else
 	{
-		printf("Custo: R$%.2f\n", 15.0*mtros_quad);
+		printf("Custo: R$%.2f\n", 15.0*area);
 	}
 	return 0;
+}
+
+int ler_int(char *txto)
+{
+	int n = 0;
+	printf("%s", txto);
+	scanf("%d", &n);
+
+	return n;
 }
