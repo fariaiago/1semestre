a0af6e945dac1d67670b8293aaabc4167cb4ae54
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-08-23 13:49:35 +0000

    Tue, Aug 23, 2022, 1:49 PM +00:00

--- C/exercicio2/C.c
+++ C/exercicio2/C.c
@@ -4,9 +4,10 @@ int ler_int(char*);
 
 int main(void)
 {
-	int rqr_mtral =ler_int("");
-	int n_irms = 0;
-	int venceu = ler_int("O boleto foi pago antes do vencimento? (\"0\" para não e \"1\" para sim)");
+	float prço = 200.0;
+	int rqr_mtral =ler_int("O aluno requer material? (\"0\" para não e \"1\" para sim): ");
+	int n_irms = ler_int("Quantos irmãos o aluno possui já matriculado?: ");
+	int venceu = ler_int("O valor foi pago antes do vencimento? (\"0\" para não e \"1\" para sim): ");
 	return 0;
 }
 
