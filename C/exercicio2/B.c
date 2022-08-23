9cd820ff8314fb066cf4b5db77ffb2bb0be697e3
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-08-23 13:53:08 +0000

    Tue, Aug 23, 2022, 1:53 PM +00:00

--- C/exercicio2/B.c
+++ C/exercicio2/B.c
@@ -4,14 +4,14 @@ int main(void)
 {
 	int tipo_tinta = 0, mtros_quad = 0;
 
-	printf("Qual o tipo de tinta a ser usada?(\"0\" para acrílica e \"1\" para lavável):");
+	printf("Qual o tipo de tinta a ser usada?(\"0\" para lavável e \"1\" para acrílica):");
 	scanf("%d", &tipo_tinta);
 
 	printf("Quantos metros quadrados serão pintados?:");
 	scanf("%d", &mtros_quad);
 
 	//Acrílica
-	if(tipo_tinta == 0)
+	if(tipo_tinta)
 	{
 		printf("Custo: R$%.2f\n", 10.0*mtros_quad);
 	}
--- C/exercicio2/C.c
+++ C/exercicio2/C.c
@@ -5,11 +5,11 @@ int ler_int(char*);
 int main(void)
 {
 	float prço = 200.0;
-	int rqr_mtral =ler_int("O aluno requer material? (\"0\" para não e \"1\" para sim): ");
+	int rqr_mtrl =ler_int("O aluno requer material? (\"0\" para não e \"1\" para sim): ");
 	int n_irms = ler_int("Quantos irmãos o aluno possui já matriculado?: ");
 	int vnceu = ler_int("O valor foi pago antes do vencimento? (\"0\" para não e \"1\" para sim): ");
 
-	if
+	if(rqr_mtrl)
 	return 0;
 }
 
