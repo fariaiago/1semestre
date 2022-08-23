16aeee5caf15232cd7ac84685c9ac01d4644e5fc
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-08-23 13:02:56 +0000

    Tue, Aug 23, 2022, 1:02 PM +00:00

--- C/exercicio1/c.c
+++ C/exercicio1/c.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
-void main(void) {
-	int n1,n2,n3;
+int main(void) {
+	int n1, n2, n3;
 	float media;
 
 	scanf("%d %d %d", &n1, &n2, &n3);
-	media = (float)(n1+n2+n3)/3;
+	media = (float) (n1 + n2 + n3) / 3;
 	printf("%.2f ", media);
-	if(media > 5){
-		printf("e maior que 5");
+	if(media > 5)
+	{
+		printf("é maior que 5\n");
 	}
-	else{
-		printf("nao e maior que 5");
+	else
+	{
+		printf("não é maior que 5\n");
 	}
 }
