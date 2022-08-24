05c8b07e00abc192ca20f40feedd2cbc4796720a
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-08-24 11:24:39 +0000

    Wed, Aug 24, 2022, 11:24 AM +00:00

--- C/aula2/AIA.c
+++ C/aula2/AIA.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-	
+	int modo = argv[1];
+	switch(modo)
+	{
+		case 0:
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+	}
 }
