066686185c168af508c1a1b801c37959d7150533
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-08-24 11:27:03 +0000

    Wed, Aug 24, 2022, 11:27 AM +00:00

--- C/aula2/AIA.c
+++ C/aula2/AIA.c
@@ -7,11 +7,17 @@ int main(int argc, char *argv[])
 	switch(modo)
 	{
 		case 0:
+			soma(argv);
 		case 1:
 		case 2:
 		case 3:
 		case 4:
 		case 5:
 		case 6:
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
 	}
 }
