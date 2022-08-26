b7e08ab487ec78b92c6309d0c9ff590a94619c73
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-08-26 14:05:47 +0000

    Fri, Aug 26, 2022, 2:05 PM +00:00

--- C/aula3/condicional/1.c
+++ C/aula3/condicional/1.c
@@ -2,7 +2,7 @@
 
 typedef struct pessoa
 {
-	char nome[20];
+	char nome[16];
 	int idade;
 	float peso;
 } pessoa;
@@ -12,6 +12,8 @@ pessoa crr_pss(char*);
 int main(void)
 {
 	pessoa p1 = crr_pss("primeira");
+	pessoa p2 = crr_pss("segunda");
+	pessoa p3 = crr_pss("terceira");
 	return 0;
 }
 
@@ -19,7 +21,7 @@ pessoa crr_pss(char *n_pssoa)
 {
 
 	pessoa p;
-	printf("Nome da %s pessoa: ", n_pssoa);
+	printf("Nome, idade e peso da %s pessoa(respectivamente e separado por espaço): ", n_pssoa);
 	scanf(" %s %d", p.nome, &(p.idade));
 	return p;
 }
