4b62d640e5bf19d48065a4c18c7537326bf02697
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-08-30 13:31:58 +0000

    Tue, Aug 30, 2022, 1:31 PM +00:00

--- C/aula3/condicional/4.c
+++ C/aula3/condicional/4.c
@@ -5,26 +5,29 @@ int main(int argc, char *argv[])
 {
 	if(argc != 4)
 	{
-		printf("Uso correto: ./4 <tipo de operação> <1° número> <2° número>\n Tipos de operação válidos: soma(0), subtrair maior por menor(1), dividir 1° por 2°(2).\n");
+		printf("Erro: Número errôneo de argumentos.\n"
+		"  Uso correto: ./4 <tipo de operação> <1° número> <2° número>\n"
+		"  Tipos de operação válidos: soma(0), subtrair maior por menor(1), dividir 1° por 2°(2).\n");
 		return 1;
 	}
 
 	int tipo = atoi(argv[1]);
-	int n1 = atof(argv[2]), n2 = atof(argv[3]);
+	float n1 = atof(argv[2]), n2 = atof(argv[3]);
 
 	switch(tipo)
 	{
 		case 0:
-			printf("Resultado: %f", n1 + n2);
+			printf("Resultado: %f\n", n1 + n2);
 			break;
 		case 1:
-			printf("Resultado: %f", n1 > n2 ? n1 - n2 : n2 - n1);
+			printf("Resultado: %f\n", n1 > n2 ? n1 - n2 : n2 - n1);
 			break;
 		case 2:
-			printf("Resultado: %f", n1 / n2);
+			printf("Resultado: %f\n", n1 / n2);
 			break;
 		default:
-			printf("Erro:Operação inválida.\nTipos de operação válidos: soma(0), subtrair maior por menor(1), dividir 1° por 2°(2).\n");
+			printf("Erro: Operação inválida.\n"
+			"  Tipos de operação válidos: soma(0), subtrair maior por menor(1), dividir 1° por 2°(2).\n");
 			return 2;
 	}
 
