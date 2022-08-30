547f08b418eaef43fe7d5c34611ebe1edd024064
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-08-30 12:10:37 +0000

    Tue, Aug 30, 2022, 12:10 PM +00:00

--- C/aula4/5.c
+++ C/aula4/5.c
@@ -2,8 +2,21 @@
 
 int main()
 {
-	for(int i = 1; i <= 20; i++)
+	for(int i = 1; i < 10; i++)
 	{
-		printf("%i\n", i);
+		if(passou(i + 1))
+		{
+			printf("Aluno");
+		}
 	}
+}
+
+int passou(int n)
+{
+	int n1 = 0, n2 = 0;
+	printf("Nota n1 e n2 do %i° aluno", n);
+	if((n1 + n2) / 2 >= 7)
+		return 0;
+
+	return 1;
 }
