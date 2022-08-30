a38645a40876c7c84c88ba13074d064776372ddc
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-08-30 11:49:18 +0000

    Tue, Aug 30, 2022, 11:49 AM +00:00

--- C/aula4/4.c
+++ C/aula4/4.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-	for(int i = 0; i < 20; i++)
+	for(int i = 0; i < 15; i++)
 	{
 		printf("%i\n", i);
 	}
@@ -10,6 +10,8 @@ int main()
 
 float ler_imc(int n_pssoa)
 {
+	int e_mlhr = 0;
+	float ps = 0, altra = 0;
 	printf("Qual o sexo(\"0\" para mulher, \"1\" para homem)peso(em kg), altura(em metros) da %i° pessoa: ", n_pssoa + 1);
-	scanf("");
+	scanf(" %d %f %f",e_mlhr, ps, altra);
 }
