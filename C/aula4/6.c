f10b48d58af60b79a0d565766ed1eb424d602ede
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-08-30 12:32:57 +0000

    Tue, Aug 30, 2022, 12:32 PM +00:00

--- C/aula4/6.c
+++ C/aula4/6.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int md(int);
+float md(int);
 
 int main()
 {
+	int n_pssou = 0;
 	float md_trma = 0, mr_nta = 0;
-	for(int i = 0; i < 2; i++)
+	for(int i = 0; i < 10; i++)
 	{
-		int n_pssou = 0;
 		float md_alno = md(i + 1);
 		md_trma += md_alno;
 		if(md_alno >= 7)
@@ -15,7 +15,7 @@ int main()
 			n_pssou++;
 			mr_nta = md_alno > mr_nta ? md_alno : mr_nta;
 		}
-		printf("Aluno n°%i%s passou, %i passaram direto, maior nota é %f e a média atual da turma é %f\n",
+		printf("Aluno n°%i%s passou, %i passaram direto, maior nota é %.2f e a média atual da turma é %.2f\n",
 			i + 1, md_alno >= 7 ? "" : " não", n_pssou, mr_nta, md_trma/(i+1));
 	}
 }
