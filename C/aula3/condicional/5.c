79d80540baf366e2594cc9710c0a49fa1433441b
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-02 11:40:33 +0000

    Fri, Sep 2, 2022, 11:40 AM +00:00

--- C/aula3/condicional/5.c
+++ C/aula3/condicional/5.c
@@ -5,15 +5,17 @@ int main(int argc, char *argv[])
 {
 	if(argc != 4)
 	{
+		printf
 		return 1;
 	}
 
 	int tpo = atoi(argv[1]), mses = atoi(argv[3]);;
 	float quntia = atof(argv[2]);
 
+	float vlor_final, taxa = tpo ? 1.05;
 	for(int i = 0; i < mses; i++)
 	{
-		
+		f
 	}
 	return 0;
 }
