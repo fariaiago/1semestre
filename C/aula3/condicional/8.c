8f3c5ac918bd7c8fbdd9ff9a73c2fd186b515881
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-05 13:46:41 +0000

    Mon, Sep 5, 2022, 1:46 PM +00:00

---
+++ C/aula3/condicional/8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void imprmr_dstno(char *, int);
+
+int main(int argc, char *argv[])
+{
+	if(argc != 2)
+	{
+		printf("Uso: ./7 <código de região>\n");
+		return 1;
+	}
+}
