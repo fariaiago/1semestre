45620a4baad832be64cda3a2d10aeecf3cf2bb7b
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-05 13:23:58 +0000

    Mon, Sep 5, 2022, 1:23 PM +00:00

--- C/aula3/condicional/6.c
+++ C/aula3/condicional/6.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
 	if(argc != 3)
 	{
+		printf("Uso: ./6 <glicemia> <HDL> (em mg/dl)\n");
 		return 1;
 	}
 
-	
+	int glcmia = atoi(argv[1]), hdl = atoi(argv[2]);
+	if(glcmia < 110)
+	{
+		printf("	Glicemia normal\n");
+	}
+	else
+	{
+		printf("	Glicemia anormal\n");
+	}
+
+	if(hdl)
+	{
+		printf("	Glicemia normal\n");
+	}
+	else
+	{
+		printf("	Glicemia anormal\n");
+	}
 
 	return 0;
 }
