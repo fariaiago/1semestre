a16be53856f23be3ca1798473a30f301ff11b3db
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-05 13:40:24 +0000

    Mon, Sep 5, 2022, 1:40 PM +00:00

--- C/aula3/condicional/7.c
+++ C/aula3/condicional/7.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void imprmr_dstno(char *, int);
 
 int main(int argc, char *argv[])
 {
+	if(argc != 2)
+	{
+		printf("Uso: ./7 <código de região>\n");
+		return 1;
+	}
+
+	int rgiao = atoi(argv[1]);
+	switch(rgiao)
+	{
+		case 1:
+			imprmr_dstno("Sul", 10);
+			break;
+		case 2:
+			imprmr_dstno("Norte", 5);
+			break;
+		case 3, 4:
+			imprmr_dstno("Leste", 8);
+			break;
+		case 5 || 6 || 7 || 8 || 9 || 10:
+			imprmr_dstno("Oeste", 9);
+			break;
+		default:
+			printf("Erro: Alíquota inválida.\n");
+			break;
+
+	}
 	return 0;
+}
+
+void imprmr_dstno(char *dstno, int alqta)
+{
+	printf("O pacote irá para a região %s, cuja alíquota é %i%%.\n", dstno, alqta);
 }
