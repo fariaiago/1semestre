78c684a244e58b96c0300b448cb78f7f3157f01b
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-06 12:29:13 +0000

    Tue, Sep 6, 2022, 12:29 PM +00:00

--- C/aula5/5.c
+++ C/aula5/5.c
@@ -13,6 +13,7 @@ int main(void)
 			//int num = 0;
 			printf("Número? ");
 			scanf(" %s", txt);
+			printf("%s %i\n", txt, strcmp(txt, "fim"));
 			if(strcmp(txt, "fim") == 0 || strcmp(txt, "FIM") == 0)
 				break;
 			else
@@ -22,6 +23,7 @@ int main(void)
 		}
 		while(cntnuar);
 		printf("Valor da compra: %i\n", soma);
+		soma = 0;
 		printf("Nova compra? (\"0\" para não, \"1\" sim) ");
 		scanf(" %d", &nv_cmpra);
 	}
