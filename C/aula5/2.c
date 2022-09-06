6c9b971204e8b842ca34ce701476c58dceba88f4
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-06 11:49:48 +0000

    Tue, Sep 6, 2022, 11:49 AM +00:00

--- C/aula5/2.c
+++ C/aula5/2.c
@@ -2,12 +2,13 @@
 
 int main(void)
 {
-	int n_idades = 0, cntnuar_exec = 0;
+	int n_idades = 0, media = 0, cntnuar_exec = 0;
 	do
 	{
 		int idade = 0;
 		printf("Qual a idade? ");
 		scanf(" %d", &idade);
+		media += idade;
 		if(idade >= 18)
 			printf("Maior de idade\n");
 		else
@@ -17,5 +18,5 @@ int main(void)
 		n_idades++;
 	}
 	while(cntnuar_exec);
-	printf("N° idades: %i\n", n_idades);
+	printf("N° de idades: %i, Média das idades: %f\n", n_idades, media*1.0/(n_idades*1.0));
 }
