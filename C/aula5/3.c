a52b2b6dd0de5ef0a07baeba3410a2872a257cc5
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-06 12:00:13 +0000

    Tue, Sep 6, 2022, 12:00 PM +00:00

--- C/aula5/3.c
+++ C/aula5/3.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-	i
+	int soma = 0, num = 0;
 	for(int i = 1; i < argc; i++)
 	{
 		if(i % 2 == 0)
 		{
 			char op = argv[i];
+			if(argv[i] == '+')
+				soma += num;
+			else
+				
+		}
+		else
+		{
+			num = atoi(argv[i]);
 		}
 	}
 }
