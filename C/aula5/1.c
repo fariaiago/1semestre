a61c9f10eab34b3fc42e4116ca0f6df81859a81a
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-06 11:41:40 +0000

    Tue, Sep 6, 2022, 11:41 AM +00:00

--- C/aula5/1.c
+++ C/aula5/1.c
@@ -2,13 +2,19 @@
 
 int main(void)
 {
-	int i = 0;
+	int i = 0, cont = 0;
 	do
 	{
-		int idade = 0, cont = 0;
+		int idade = 0;
 		printf("Qual a idade? ");
 		scanf(" %d", &idade);
+		if(idade >= 18)
+			printf("Maior de idade\n");
+		else
+			printf("Menor de idade\n");
+		printf("Continuar? (0 não, 1 sim) ");
+		scanf(" %d", &cont);
 		i++;
 	}
-	while();
+	while(cont);
 }
