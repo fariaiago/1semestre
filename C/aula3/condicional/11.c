23689ccb7f32dd7daa626a54c23c70e3fb0ba450
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-06 13:34:18 +0000

    Tue, Sep 6, 2022, 1:34 PM +00:00

--- C/aula3/condicional/11.c
+++ C/aula3/condicional/11.c
@@ -3,13 +3,25 @@
 
 int main(int argc, char *argv[])
 {
-	if(argc != 6)
+	if(argc != 5)
 	{
-		printf("Uso: ./10 <Preço das compras> <Número de cômodos> <Projeto de decoração> <Projeto estrutural> <Comprado pela internet>\n"
-			"Projeto de decoração: \"0\" para não, \"1\" para sim\n"
-			"Projeto estrutural: \"0\" para não, \"1\" para sim\n"
-			"Comprado pela internet: \"0\" para não, \"1\" para sim\n");
+		printf("Uso: ./10 <Preço das compras> <Região> <Dia de promoção> <Cliente VIP>\n"
+			"Região: \"0\" para Sul/Sudeste, \"1\" para outras\n"
+			"Dia de promoção: \"0\" para não, \"1\" para sim\n"
+			"Cliente VIP: \"0\" para não, \"1\" para sim\n");
 		return 1;
 	}
+	float vlor = atof(argv[1]);
+	int rg = atoi(argv[2]), promo = atoi(argv[3]), vip = atoi(argv[4]);
+	if(!promo)
+	{
+		if(rg)
+			vlor = vlor*1.1;
+		else
+			vlor = vlor*1.2;
+	}
+	if(vip)
+		vlor -= 100;
+	printf("Valor: %.2f\n", vlor);
 	return 0;
 }
