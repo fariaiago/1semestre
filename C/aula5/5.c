74aff4a7c78abf777e5d69a14ed5da82e6d326f7
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-06 12:17:52 +0000

    Tue, Sep 6, 2022, 12:17 PM +00:00

---
+++ C/aula5/5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int soma = 0, reinciar = 0; cntnuar = 0;
+	do
+	{
+		r
+	}
+	while(reinciar);
+	do
+	{
+		int num = 0;
+		printf("Número? ");
+		scanf(" %d", &num);
+		soma += num;
+		printf("Continuar? (\"0\" para não, \"+\" sim) ");
+		scanf(" %c", &cntnuar);
+	}
+	while(cntnuar_exec == '+');
+	printf("Soma: %i\n", soma);
+
+	return 0;
+}
