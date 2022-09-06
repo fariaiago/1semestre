b3a1e43c5cb5a23761c713f7329e564de3cef2ed
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-06 13:21:57 +0000

    Tue, Sep 6, 2022, 1:21 PM +00:00

--- C/aula3/condicional/10.c
+++ C/aula3/condicional/10.c
@@ -10,5 +10,14 @@ int main(int argc, char *argv[])
 			"Projeto estrutural: \"0\" para não, \"1\" para sim\n");
 		return 1;
 	}
+	float vlor =  0;
+	int area = atoi(argv[1]), n_cmds = atoi(argv[2]), deco = atoi(argv[3]), strut = atoi(argv[4]);
+	if(deco || strut)
+		vlor = area * 30;
+	else
+		vlor = area * 20;
+
+	if(deco)
+		vlor += n_cmds * 600;
 	return 0;
 }
