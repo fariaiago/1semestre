7024280f4f446a9a9e0dbd7bd8fd2a731030a890
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-06 12:23:17 +0000

    Tue, Sep 6, 2022, 12:23 PM +00:00

--- C/aula5/5.c
+++ C/aula5/5.c
@@ -3,23 +3,28 @@
 
 int main(void)
 {
-	int soma = 0, nv_cmpra = 0; cntnuar = 0;
+	int soma = 0, nv_cmpra = 0, cntnuar = 0;
 	do
 	{
-		r
+		do
+		{
+			char *txt;
+			//int num = 0;
+			printf("Número? ");
+			scanf(" %s", txt);
+			if(txt == "fim" || txt == "FIM")
+				break;
+			else
+				soma += atoi(txt);
+			//printf("Continuar? (\"0\" para não, \"1\" sim) ");
+			//scanf(" %c", &cntnuar);
+		}
+		while(cntnuar);
+		printf("Valor da compra: %i\n", soma);
+		printf("Nova compra? (\"0\" para não, \"1\" sim) ");
+		scanf(" %c", &nv_cmpra);
 	}
 	while(nv_cmpra);
-	do
-	{
-		int num = 0;
-		printf("Número? ");
-		scanf(" %s", &num);
-		soma += num;
-		printf("Continuar? (\"0\" para não, \"1\" sim) ");
-		scanf(" %c", &cntnuar);
-	}
-	while(cntnuar_exec == '+');
-	printf("Soma: %i\n", soma);
 
 	return 0;
 }
