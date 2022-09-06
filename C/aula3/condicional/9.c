66f12edfc0507d0921cb955478b34ff1390b14e6
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-06 13:01:29 +0000

    Tue, Sep 6, 2022, 1:01 PM +00:00

--- C/aula3/condicional/9.c
+++ C/aula3/condicional/9.c
@@ -3,13 +3,14 @@
 
 int main(int argc, char *argv[])
 {
-	if(argc != 3)
+	if(argc != 4)
 	{
-		printf("Uso: ./9 <Região> <Se lote é de esquina>\n"
-			"Regiões: \"0\" para Norte, \"1\" para Sul\n");
+		printf("Uso: ./9 <área> <Região> <Se o lote é de esquina>\n"
+			"Regiões: \"0\" para Norte, \"1\" para Sul\n"
+			"De esquina: \"0\" para não, \"1\" para sim\n");
 		return 1;
 	}
-	double saldo_medio = atof(argv[1]);
+	int area = atoi(argv[1]), rg = atoi(argv[2]), esq = atoi(argv[3]);
 	if(saldo_medio < 250.0)
 	{
 		printf("Saldo médio: %.2f, Crédito: %.2f\n", saldo_medio, saldo_medio*1.05);
