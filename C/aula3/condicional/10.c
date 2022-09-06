a4a3f6246d31a0c6ac58aa997d85dc2c4a149111
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-06 13:25:20 +0000

    Tue, Sep 6, 2022, 1:25 PM +00:00

--- C/aula3/condicional/10.c
+++ C/aula3/condicional/10.c
@@ -3,21 +3,28 @@
 
 int main(int argc, char *argv[])
 {
-	if(argc != 4)
+	if(argc != 6)
 	{
-		printf("Uso: ./10 <Área> <Número de cômodos> <Projeto de decoração> <Projeto estrutural>\n"
+		printf("Uso: ./10 <Área> <Número de cômodos> <Projeto de decoração> <Projeto estrutural> <Comprado pela internet>\n"
 			"Projeto de decoração: \"0\" para não, \"1\" para sim\n"
-			"Projeto estrutural: \"0\" para não, \"1\" para sim\n");
+			"Projeto estrutural: \"0\" para não, \"1\" para sim\n"
+			"Comprado pela internet: \"0\" para não, \"1\" para sim\n");
 		return 1;
 	}
 	float vlor =  0;
-	int area = atoi(argv[1]), n_cmds = atoi(argv[2]), deco = atoi(argv[3]), strut = atoi(argv[4]);
+	int area = atoi(argv[1]), n_cmds = atoi(argv[2]), deco = atoi(argv[3]), strut = atoi(argv[4]), net = atoi(argv[5]);
 	if(deco || strut)
 		vlor = area * 30;
 	else
 		vlor = area * 20;
 
 	if(deco)
-		vlor += n_cmds * 600;
+		vlor += n_cmds * 100;
+	if(strut)
+		vlor += 1200;
+	if(net)
+		vlor = vlor * 0.95;
+
+	printf("Valor: R$%.2f\n", vlor);
 	return 0;
 }
