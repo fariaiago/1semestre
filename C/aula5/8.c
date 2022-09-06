b7e288e843373282b75bd59aa1f6f691d5b7f889
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-06 12:47:39 +0000

    Tue, Sep 6, 2022, 12:47 PM +00:00

--- C/aula5/8.c
+++ C/aula5/8.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-void tab5_for();
-void tab5_while();
+void tab_for();
+void tab_while();
 
 int main(void)
 {
-	printf("Tabuada do 5\n");
-	tab5_while();
+	printf("Tabuada do 2 ao 10\n");
+	tab_while();
 	return 0;
 }
 
-void tab5_for()
+void tab_for()
 {
 	for(int i = 0; i <= 10; i++)
 	{
@@ -18,12 +18,20 @@ void tab5_for()
 	}
 }
 
-void tab5_while()
+void tab_while()
 {
 	int i = 0;
 	while(i <= 10)
 	{
 		printf("5 x %i = %i\n", i, i*5);
 		i++;
+	}
+}
+
+void for_2a10()
+{
+	for(int i = 2; i <= 10; i++)
+	{
+		a
 	}
 }
