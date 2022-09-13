da7f41eb2fc192de79bf4254cf948a637736dfdd
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-13 11:37:01 +0000

    Tue, Sep 13, 2022, 11:37 AM +00:00

--- C/aula6/2.c
+++ C/aula6/2.c
@@ -5,31 +5,32 @@ int main(void)
 	int exec = 0;
 	do
 	{
-		a
+		int lds[3];
+		printf("Qual os lados do triângulo? ");
+		scanf(" %d %d %d", &lds[0], &lds[1], &lds[2]);
+
+		if(!(lds[0] < lds[1] + lds[2]) || !(lds[1] < lds[0] + lds[2]) || !(lds[2] < lds[0] + lds[1]))
+		{
+			printf("Erro: não é triângulo\n");
+			return 1;
+		}
+
+		if(lds[0] == lds[1] && lds[0] == lds[2])
+		{
+			printf("Triângulo equilátero\n");
+		}
+		else if(lds[0] == lds[1] || lds[0] == lds[2] || lds[1] == lds[2])
+		{
+			printf("Triângulo isóceles\n");
+		}
+		else
+		{
+			printf("Triângulo escaleno.\n");
+		}
+		printf("Continuar? \"0\" não, \"1\" sim: ");
+		scanf(" %d", &exec);
 	}
 	while(exec);
-	int lds[3];
-	printf("Qual os lados do triângulo? ");
-	scanf(" %d %d %d", &lds[0], &lds[1], &lds[2]);
-
-	if(!(lds[0] < lds[1] + lds[2]) || !(lds[1] < lds[0] + lds[2]) || !(lds[2] < lds[0] + lds[1]))
-	{
-		printf("Erro: não é triângulo\n");
-		return 1;
-	}
-
-	if(lds[0] == lds[1] && lds[0] == lds[2])
-	{
-		printf("Triângulo equilátero\n");
-	}
-	else if(lds[0] == lds[1] || lds[0] == lds[2] || lds[1] == lds[2])
-	{
-		printf("Triângulo isóceles\n");
-	}
-	else
-	{
-		printf("Triângulo escaleno.\n");
-	}
 
 	return 0;
 }
