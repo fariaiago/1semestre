9982971e124f5d9242a2b2f6e46658c724ad7e78
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-13 12:53:15 +0000

    Tue, Sep 13, 2022, 12:53 PM +00:00

--- C/aula6/5.c
+++ C/aula6/5.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
 	if(argc != 4)
 	{
-		printf("Uso: <Estadia> <Número de passagems> <Translado aeroporto-hotel>\n");
+		printf("Uso: <Número de pessoas no grupo> <Estadia> <Passagem> <Translado aeroporto-hotel>\n"
+			"Estadia: Número de dias\n"
+			"Passagem: Número de milhas\n"
+			"Translado: \"0\" para não, \"1\" para sim\n");
+			return 1;
 	}
+	int n_pssoa = atoi(argv[1]), n_dias = atoi(argv[2]), mlhas = atoi(argv[3]), trans = atoi(argv[4]);
+	float vlor = (100*n_pssoa*n_dias) + (50*mlhas) 
+	return 0;
 }
