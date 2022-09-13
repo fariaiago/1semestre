5350151d7420ee256ccac9a875678917036ebc24
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-13 13:19:30 +0000

    Tue, Sep 13, 2022, 1:19 PM +00:00

--- C/aula6/5.c
+++ C/aula6/5.c
@@ -7,8 +7,17 @@ int main(void)
 	{
 		int n_pssoa = 0, n_dias = 0, mlhas = 0, trans = 0;
 		printf("Tamanho do grupo, estadia, em dias, n° milhas e se translado aeroporto-hotel: ");
-		scanf("");
+		scanf(" %d %d %d %d", &n_pssoa, &n_dias, &mlhas, &trans);
 		float vlor = (100*n_pssoa*n_dias) + (50*mlhas) + (trans ? 60 : 0);
+		if(n_pssoa > 10)
+		{
+			vlor *= 0.9;
+		}
+		else if (n_pssoa >= 4)
+		{
+			vlor *= 0.95;
+		}
+		printf("Preço final: R$%.2f\n", vlor);
 		printf("Que fazer outro pacote?\"0\" não, \"1\" sim: ");
 		scanf(" %d", &exec);
 	}
