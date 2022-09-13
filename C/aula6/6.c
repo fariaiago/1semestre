c6177370df42e3eca79eba1e24a98113dbf20df8
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-13 13:42:52 +0000

    Tue, Sep 13, 2022, 1:42 PM +00:00

--- C/aula6/6.c
+++ C/aula6/6.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float nv_prç(float);
+char *clsse(float);
+
 int main(int argc, char *argv[])
 {
 	if(argc == 1)
@@ -11,5 +14,38 @@ int main(int argc, char *argv[])
 	for(int i = 1; i < argc; i++)
 	{
 		float prç = atof(argv[i]);
+		float nv = nv_prç(prç);
+		printf("Produto %i° custava R$%.2f e passará a custar R$%.2f, agora classificado como %s.\n", i, prç, nv, clsse(nv));
+	}
+	return 0;
+}
+
+float nv_prç(float prç)
+{
+	if(prç < 50)
+		return prç*1.1;
+	else if(prç > 500)
+		return prç*1.3;
+	else
+		return prç*1.2;
+}
+
+char *clsse(float prç)
+{
+	if(prç < 80)
+	{
+		return "barato";
+	}
+	else if(prç > 800)
+	{
+		return "muito caro";
+	}
+	else if(prç >= 80 && prç <= 600)
+	{
+		return "normal";
+	}
+	else
+	{
+		return "caro";
 	}
 }
