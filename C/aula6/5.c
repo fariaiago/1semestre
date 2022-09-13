b003aa2150bd403940ef6a38da0b73dd323130c3
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-13 13:12:40 +0000

    Tue, Sep 13, 2022, 1:12 PM +00:00

--- C/aula6/5.c
+++ C/aula6/5.c
@@ -2,15 +2,12 @@
 
 int main(void)
 {
-	if(argc != 4)
-	{
-			return 1;
-	}
 	int exec = 0;
 	do
 	{
 		int n_pssoa = 0, n_dias = 0, mlhas = 0, trans = 0;
 		printf("Tamanho do grupo, estadia, em dias, n° milhas e se translado aeroporto-hotel: ");
+		scanf("");
 		float vlor = (100*n_pssoa*n_dias) + (50*mlhas) + (trans ? 60 : 0);
 		printf("Que fazer outro pacote?\"0\" não, \"1\" sim: ");
 		scanf(" %d", &exec);
