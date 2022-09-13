bbaac652b99b032a623fe8065bccc7ffffbc181f
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-13 13:07:11 +0000

    Tue, Sep 13, 2022, 1:07 PM +00:00

--- C/aula6/5.c
+++ C/aula6/5.c
@@ -4,16 +4,13 @@ int main(void)
 {
 	if(argc != 4)
 	{
-		printf("Uso: <Número de pessoas no grupo> <Estadia> <Passagem> <Translado aeroporto-hotel>\n"
-			"Estadia: Tempo dias\n"
-			"Passagem: Número de milhas\n"
-			"Translado: \"0\" para não, \"1\" para sim\n");
 			return 1;
 	}
 	int exec = 0;
 	do
 	{
-		int n_pssoa = atoi(argv[1]), n_dias = atoi(argv[2]), mlhas = atoi(argv[3]), trans = atoi(argv[4]);
+		int n_pssoa = 0, n_dias = 0, mlhas = 0, trans = 0;
+		printf("Tamanho do grupo, estadia, em dias, n° milhas e se translado aeroporto-hotel: ");
 		float vlor = (100*n_pssoa*n_dias) + (50*mlhas) + (trans ? 60 : 0);
 		printf("Que fazer outro pacote?\"0\" não, \"1\" sim: ");
 		scanf(" %d", &exec);
