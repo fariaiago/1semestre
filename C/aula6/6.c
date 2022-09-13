d9b1ffe7c0311c8a4eb08c9442313a433b17ecc2
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-13 13:30:10 +0000

    Tue, Sep 13, 2022, 1:30 PM +00:00

--- C/aula6/6.c
+++ C/aula6/6.c
@@ -1,1 +1,15 @@
-%
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	if(argc == 1)
+	{
+		printf("Uso: ./6 <Preco antigo> <do> <produto> <...>\n");
+		return 1;
+	}
+	for(int i = 1; i < argc; i++)
+	{
+		float prç = atof(argv[i]);
+	}
+}
