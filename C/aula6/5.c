9fccc75caad959feb03c99e06b133dbbb25cdacb
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-13 12:56:42 +0000

    Tue, Sep 13, 2022, 12:56 PM +00:00

--- C/aula6/5.c
+++ C/aula6/5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+int main(void)
 {
 	if(argc != 4)
 	{
@@ -11,7 +11,14 @@ int main(int argc, char *argv[])
 			"Translado: \"0\" para não, \"1\" para sim\n");
 			return 1;
 	}
+	int exec = 0;
+	do
+	{
+		printf("Que fazer outro pacote?\"0\" não, \"1\" sim: ");
+		scanf(" %d", &exec);
+	}
+	while(exec);
 	int n_pssoa = atoi(argv[1]), n_dias = atoi(argv[2]), mlhas = atoi(argv[3]), trans = atoi(argv[4]);
-	float vlor = (100*n_pssoa*n_dias) + (50*mlhas) 
+	float vlor = (100*n_pssoa*n_dias) + (50*mlhas) + (trans ? 60 : 0);
 	return 0;
 }
