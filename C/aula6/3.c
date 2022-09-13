cb27a4757aec435a76fe28911c9699dfafe3f93c
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-13 11:52:30 +0000

    Tue, Sep 13, 2022, 11:52 AM +00:00

--- C/aula6/3.c
+++ C/aula6/3.c
@@ -3,9 +3,25 @@
 
 int main(int argc, char *argv[])
 {
-	for(int i = 0; i < argc, i++)
+	for(int i = 1; i < argc; i++)
 	{
-		float vlor_md = atod(argv[i]);
-		printf("");
+		float sld_md = atof(argv[i]);
+		printf("Valor médio: R$%.2f. ", sld_md);
+		if(sld_md <= 500)
+		{
+			printf("Valor do crédito: R$%.2f.\n", sld_md*0);
+		}
+		else if(sld_md <= 1000)
+		{
+			printf("Valor do crédito: R$%.2f.\n", sld_md*0.3);
+		}
+		else if(sld_md <= 3000)
+		{
+			printf("Valor do crédito: R$%.2f.\n", sld_md*0.4);
+		}
+		else
+		{
+			printf("Valor do crédito: R$%.2f.\n", sld_md*0.5);
+		}
 	}
 }
