fe054dbe1e1a4c33ef7fe5f12bd76ebf0f17c7b8
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-20 11:30:50 +0000

    Tue, Sep 20, 2022, 11:30 AM +00:00

---
+++ C/aula7/ativ.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n_atend = 0, cntnr = 0;
+	do
+	{
+		char nome[16];
+		printf("Qual a prossima pessoa a ser atendida?")
+		scanf(" %s", nome);
+		printf("%s, senha %2d", nome, n_atend);
+		printf("Continuar? ");
+		scanf(" %d ", &cntnr);
+		n_atend++;
+	}
+	while(cntnr && n_atend < 50);
+	return 0;
+}
