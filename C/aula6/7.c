e27220ffd9bcede4e24b429b4ad4101eef61d58b
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-20 12:53:23 +0000

    Tue, Sep 20, 2022, 12:53 PM +00:00

--- C/aula6/7.c
+++ C/aula6/7.c
@@ -13,14 +13,13 @@ int main(int argc, char *argv[])
 		float n_alnos = 0, n_ausc = 0;
 		char *turma = argv[i];
 		int cnt = -1;
-		while(turma[cnt++])
+		while(turma[++cnt])
 		{
 			n_ausc += turma[cnt] == 'A' || turma[cnt] == 'a' ? 1 : 0;
 			n_alnos++;
 		}
-		if((n_ausc/n_alnos)*100.0f >= 5)
-			printf("Turma %d° teve ausencia superior a 5%(%f)\n", i, (n_ausc/n_alnos)*100.0f);
+		if((n_ausc/n_alnos)*100.0 >= 5)
+			printf("Turma %d° teve ausencia superior a 5%%(%f%%)\n", i, (n_ausc/n_alnos)*100.0);
 	}
- 	printf("Presença: %f%%.\n", (1.0*n_prsnç)/(1.0*argc)*100.0);
 	return 0;
 }
