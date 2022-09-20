127f5572320d68a7839f5152cc1587863b6f6ca9
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-20 12:42:26 +0000

    Tue, Sep 20, 2022, 12:42 PM +00:00

--- C/aula6/7.c
+++ C/aula6/7.c
@@ -10,14 +10,14 @@ int main(int argc, char *argv[])
 
 	for(int i = 1; i < argc; i++)
 	{
-		int n_prsnç = 0;
+		int n_prsnç = 0,;
 		char *turma = argv[i];
 		int cnt = -1;
 		while(turma[cnt++])
 		{
 			n_prsnç += turma[cnt] == 'P' || turma[cnt] == 'p' ? 1 : 0;
 		}
-		printf
+		printf("Turma %d° tem presença ");
 	}
  	printf("Presença: %f%%.\n", (1.0*n_prsnç)/(1.0*argc)*100.0);
 	return 0;
