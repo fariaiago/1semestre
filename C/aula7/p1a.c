d527f9b2349e94a45a6696929cc9117643b091cd
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-20 13:41:43 +0000

    Tue, Sep 20, 2022, 1:41 PM +00:00

--- C/aula7/p1a.c
+++ C/aula7/p1a.c
@@ -5,9 +5,19 @@ typedef struct atleta
 	int idade;
 	float oxi_sang[9];
 	float freq_card[8];
-} atleta
+} atleta;
+
+atleta criar_atleta();
 
 int main(void)
 {
-	a
+	atleta atletas[10];
+	for(int i = 0; i < 10; i++)
+		atletas[i] = criar_atleta();
+}
+
+atleta criar_atleta()
+{
+	atleta a;
+	
 }
