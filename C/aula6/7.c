79e59cd71b7447d60c164b5f50e893b318e3a70b
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-20 12:41:05 +0000

    Tue, Sep 20, 2022, 12:41 PM +00:00

--- C/aula6/7.c
+++ C/aula6/7.c
@@ -7,11 +7,17 @@ int main(int argc, char *argv[])
 		printf("Uso: ./7 <Presença> <de uma> <turma> <...>\n");
 		return 1;
 	}
-	int n_prsnç = 0;
+
 	for(int i = 1; i < argc; i++)
 	{
+		int n_prsnç = 0;
 		char *turma = argv[i];
-		n_prsnç += prsnç == 'P' || prsnç == 'p' ? 1 : 0;
+		int cnt = -1;
+		while(turma[cnt++])
+		{
+			n_prsnç += turma[cnt] == 'P' || turma[cnt] == 'p' ? 1 : 0;
+		}
+		printf
 	}
  	printf("Presença: %f%%.\n", (1.0*n_prsnç)/(1.0*argc)*100.0);
 	return 0;
