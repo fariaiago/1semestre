17f5a23b59202aa48807fb04d93a61f41830351f
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-27 12:25:46 +0000

    Tue, Sep 27, 2022, 12:25 PM +00:00

--- C/aula6/9.c
+++ C/aula6/9.c
@@ -3,5 +3,11 @@
 
 int main(int argc, char *argv[])
 {
-	
+	if(argc != 3)
+	{
+		printf("Uso: ./9 <Salário> <Número de filhos>\n");
+		return 1;
+	}
+	float salario = atof(argv[1]);
+	int n_filhos = atoi(argv[2]);
 }
