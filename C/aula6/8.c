56d2b3a720bb823900e333986e8f47c340ecd984
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-27 12:14:37 +0000

    Tue, Sep 27, 2022, 12:14 PM +00:00

--- C/aula6/8.c
+++ C/aula6/8.c
@@ -2,17 +2,26 @@
 
 int main(void)
 {
-	float md_temp = 0, n_temp = 0;
+	float md_temp = 0, n_temp = 0, i_pluvio = 0;
+
 	while(n_temp < 12)
 	{
 		float temp = 0;
-		printf("Temperatura: ");
-		scanf(" %d", &temp);
+		printf("Escreva uma medição de temperatura: ");
+		scanf(" %f", &temp);
 		if(temp <= -100)break;
 		md_temp += temp;
 		n_temp++;
 	}
 
 	for(int i = 0; i < 12; i++)
+	{
+		float pluv = 0;
+		printf("Escreva um indice pluviométrico: ");
+		scanf(" %f", &pluv);
+		if(pluv > i_pluvio)i_pluvio = pluv;
+	}
+
+	printf("Média de temperaturas: %f,maior indice pluviométrico: %f", md_temp/n_temp, i_pluvio);
 	return 0;
 }
