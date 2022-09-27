c26ea3a30e75e856a7f57bb1c14373d5d1170831
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-09-27 11:56:55 +0000

    Tue, Sep 27, 2022, 11:56 AM +00:00

--- C/aula6/8.c
+++ C/aula6/8.c
@@ -2,12 +2,17 @@
 
 int main(void)
 {
-	float md_temp = 0,n_temp = 0;
+	float md_temp = 0, n_temp = 0;
 	while(n_temp < 12)
 	{
+		float temp = 0;
 		printf("Temperatura: ");
-		scanf();
-		if()break;
+		scanf(" %d", &temp);
+		if(temp <= -100)break;
+		md_temp += temp;
+		n_temp++;
 	}
+
+	for(int i = 0; i < 12; i++)
 	return 0;
 }
