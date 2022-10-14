2b12b8be2adf71f3ff1a946e2afd2fd59232ce79
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-10-14 12:44:59 +0000

    Fri, Oct 14, 2022, 12:44 PM +00:00

--- C/aula10/3.c
+++ C/aula10/3.c
@@ -2,15 +2,16 @@
 
 int main(void)
 {
-	int maior = -99999999
+	int maior = -2147483648;
 	int num[15];
 	for(int i = 0; i < 15; i++)
 	{
 		int n = 0;
 		scanf( "%d", &n);
 		num[i] = n;
+		if(n > maior)maior = n;
 	}
-
+	printf("Maior número: %i\n", maior);
 	for(int i = 0; i < 15; i++)
 	{
 		printf("%d ", num[15 - i - 1]);
