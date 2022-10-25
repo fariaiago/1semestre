4c78326c483af08ed268c1e763d93f65dd44e484
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-10-25 11:49:23 +0000

    Tue, Oct 25, 2022, 11:49 AM +00:00

--- C/aula11/2.c
+++ C/aula11/2.c
@@ -12,5 +12,13 @@ int main(void)
 			scanf(" %d", &(notas[i][j]));
 		}
 	}
+	for(int i = 0; i < 3: i++)
+	{
+		for(int j = 0; j < 30; j++)
+		{
+			printf("Nota do %d° aluno da %d° turma: ", j+1, i+1);
+			scanf(" %d", &(notas[i][j]));
+		}
+	}
 	return 0;
 }
