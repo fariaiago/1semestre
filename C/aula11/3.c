0c01866865f995269d65125bd53e9871549aa713
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-10-25 12:41:54 +0000

    Tue, Oct 25, 2022, 12:41 PM +00:00

--- C/aula11/3.c
+++ C/aula11/3.c
@@ -3,6 +3,16 @@
 int main(void)
 {
 	int idades[10];
-	
+	int freq[10][8];
+	float sang[10][8];
+	for(int i = 0; i < 10; i++)
+	{
+		printf("Idade do %d° atleta", i+1);
+		scanf(" %d", &(idades[i]));
+		for(int j = 0; j < 8; j++)
+		{
+			
+		}
+	}
 	return 0;
 }
