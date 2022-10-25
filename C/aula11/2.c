236722cec09f3724e198b8d5ce80617a817c7b92
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-10-25 11:48:16 +0000

    Tue, Oct 25, 2022, 11:48 AM +00:00

---
+++ C/aula11/2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float notas[3][30];
+	a
+	for(int i = 0; i < 3: i++)
+	{
+		for(int j = 0; j < 30; j++)
+		{
+			printf("Nota do %d° aluno da %d° turma: ", j+1, i+1);
+			scanf(" %d", &(notas[i][j]));
+		}
+	}
+	return 0;
+}
