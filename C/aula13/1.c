3ded52d567f85ce77f4dc77c01028bb2ac1765e6
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-11-08 11:28:47 +0000

    Tue, Nov 8, 2022, 11:28 AM +00:00

--- C/aula13/1.c
+++ C/aula13/1.c
@@ -1,1 +1,13 @@
-#include <>
+#include <stdio.h>
+
+int main(void)
+{
+	int vaxx[5][20];
+	for(int i = 0; i < 20; i++)
+	{
+		int p0 = 0, p1 = 0, p2 = 0, p3 = 0, p4 = 0;
+		printf("Vacinações no %d° dia: ", i+1);
+		scanf(" %d %d %d %d ", &p0 = 0, &p1, &p2, &p3, &p4);
+	}
+	return 0;
+}
