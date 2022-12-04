e83d747c763576f2e0ce915b4b5bbb20ded2f630
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-12-04 12:59:41 +0000

    Sun, Dec 4, 2022, 12:59 PM +00:00

--- C/aapn2.c
+++ C/aapn2.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
-enum produtos = {}
+enum produtos {AC3}
 //Struct para representar um pedido
 int main(void)
 {
 	//Ler CSV e transforma-lo em matrix
 	int var = ler_dados();
+	do
+	{
+		//Fazer relatórios
+	}
+	while(1);
 	return 0;
 }
