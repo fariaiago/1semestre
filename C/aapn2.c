c5b41bcd521fbea382cd01dc777e2a8ec252f8c7
x64-UFT <111879008+x64-UFT@users.noreply.github.com>
2022-12-04 13:18:18 +0000

    Sun, Dec 4, 2022, 1:18 PM +00:00

--- C/aapn2.c
+++ C/aapn2.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 
 //Tipos de produtos disponíveis
-enum produtos {AC3 , AC2 , REJUNTE}
+enum produtos {AC3 , AC2 , REJUNTE};
+
+//Destinos de entrega
+enum locais {PALMAS, PORTO_NACIONAL};
 
 //Struct para representar um pedido
 typedef struct pedido
 {
 	enum produtos tipo;
+	enum locais destino;
 	int quantidade;
 } pedido;
+
 int main(void)
 {
 	//Ler CSV e transforma-lo em matrix
-	int var = ler_dados();
+	pedido pedidos[2][10] = ler_dados();
 	do
 	{
 		//Fazer relatórios
 	}
-	while(1); //Fzer outro caso o usuário quiser
+	while(1); //Fazer outro caso o usuário quiser
 	return 0;
 }
